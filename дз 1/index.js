5c09bc7fe11fc206ade8fefffe66f29ccc029361
Viktor-Fedor <61902011+Viktor-Fedor@users.noreply.github.com>
2020-09-26 15:29:45 +0300

    дз 1
    федор віктор

---
+++ дз 1/index.js
@@ -0,0 +1,7 @@
+var name = 'Генератор захисного поля';
+var price = 1000;
+var messege = 'Обрано ' + name + ', ціна за штуку ' + price + ' кредитів';
+console.log(messege);
+var price = 2000;
+var messege = 'Обрано ' + name + ', ціна за штуку ' + price + ' кредитів';
+console.log(messege);
