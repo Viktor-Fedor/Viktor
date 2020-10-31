3b12414492773beddd4bf9417277719376c3d796
Viktor-Fedor <61902011+Viktor-Fedor@users.noreply.github.com>
2020-10-31 16:47:45 +0200

    дз 7
    дз 7

---
+++ дз (7)/index.js
@@ -0,0 +1,21 @@
+const findLongestWord = function (string) {
+    let str = string.split(" ");
+    let longest = 0;
+    let word = null;
+    for (let i = 0; i < str.length; i +=1 ) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+  };
+  
+  /*
+   * Виклич функції для перевірки працездатності твоєї реалізації.
+   */
+  console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+  
+  console.log(findLongestWord('Google do a roll')); // 'Google'
+  
+  console.log(findLongestWord('May the force be with you')); // 'force'
