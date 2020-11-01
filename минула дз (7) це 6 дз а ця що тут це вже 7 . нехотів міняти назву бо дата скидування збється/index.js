6bbad86b862fa6e6b578b1df702a7c93c3f0fc92
Viktor-Fedor <61902011+Viktor-Fedor@users.noreply.github.com>
2020-11-01 14:40:13 +0200

    дз 7 минула дз 7 це дз 6
    дз 7 минула дз 7 це дз 6

---
+++ минула дз (7) це 6 дз а ця що тут це вже 7 . нехотів міняти назву бо дата скидування збється/index.js
@@ -0,0 +1,22 @@
+const user = {
+  changeName(str) {
+  this.name = str}
+};
+
+
+
+user.name  = "John";
+console.log(user.name);
+
+user.surname = "Smith";
+console.log(user.surname);
+
+user.name  = "Pete";
+console.log(user.name);
+
+delete user.name;
+console.log(user.name);
+
+
+
+
